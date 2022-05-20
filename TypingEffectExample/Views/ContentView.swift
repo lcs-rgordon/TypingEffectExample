aaf9ee610779d0c45c03059d10c11aafe9c353dd
Russell Gordon <github@russellgordon.ca>
2022-05-20 12:29:47 -0400

    Adjust order of modifiers and format comment.

--- TypingEffectExample/Views/ContentView.swift
+++ TypingEffectExample/Views/ContentView.swift
@@ -17,11 +17,11 @@ struct ContentView: View {
     // MARK: Computed properties
     var body: some View {
         Text("")
-        // To learn how to add custom fonts, see:
-        // https://betterprogramming.pub/swiftui-basics-importing-custom-fonts-b6396d17424d
-        // NOTE: Be sure to remove license.txt files from the list of files that are copied into the app bundle.
-        //       Multiple files with the same name will create a compile time error.
             .typeOn(message: messageToType)
+            // To learn how to add custom fonts, see:
+            // https://betterprogramming.pub/swiftui-basics-importing-custom-fonts-b6396d17424d
+            // NOTE: Be sure to remove license.txt files from the list of files that are copied into the app bundle.
+            //       Multiple files with the same name will create a compile time error.
             .font(Font.custom("kongtext", size: 24))
             .padding()
     }
