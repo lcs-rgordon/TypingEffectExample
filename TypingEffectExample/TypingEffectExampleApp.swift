836a2bd970cd74020f76455d2007cb975023e7be
Russell Gordon <github@russellgordon.ca>
2022-05-20 10:43:16 -0400

    Initial Commit

---
+++ TypingEffectExample/TypingEffectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypingEffectExampleApp.swift
+//  TypingEffectExample
+//
+//  Created by Russell Gordon on 2022-05-20.
+//
+
+import SwiftUI
+
+@main
+struct TypingEffectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
