cd3e087b6b649f4e3e4abfd1102f3191e2124e24
Russell Gordon <github@russellgordon.ca>
2022-05-20 10:46:44 -0400

    Message shows in a regular text view.

--- TypingEffectExample/ContentView.swift
+++ TypingEffectExample/ContentView.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
+// ADAPTED FROM:
+// https://medium.com/@cboynton/achieving-a-type-on-text-effect-in-swift-6934b683d1e9
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    let message = "Come with me, and see what has been foretold..."
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
+        Text(message)
             .padding()
     }
 }
